c321b2ea624dfae5c8320dc98ea0a79e7422fa69
puth88 <133973000+PutraAndika88@users.noreply.github.com>
2023-09-27 13:14:44 +0700

    Update streamlit.py
    fix

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("/content/func.py")
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import seaborn as sns
 import streamlit as st
 import urllib
